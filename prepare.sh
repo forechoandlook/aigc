6ccb15799af7a550c13ee2738f6137811d8311c5
wangyang zuo <wangyangzuo@wangyangdeMacBook-Pro.local>
2023-07-05 09:46:22 +0800

    fix

--- prepare.sh
+++ prepare.sh
@@ -5,5 +5,5 @@ mkdir models
 mkdir models/basic
 python3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/iCUZMB1NF && unzip controlnet.zip && rm -rf controlnet.zip && mv controlnet models/
 python3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/SzHkJPo06 && unzip deliberate-lora_pixarStyleLora_lora128-unet-2 && rm -rf deliberate-lora_pixarStyleLora_lora128-unet-2.zip && mv deliberate-lora_pixarStyleLora_lora128-unet-2 models/basic/
-ptyhon3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/1Z4DdzGsk && unzip other.zip && rm -rf other.zip && mv other models/
+python3 -m dfn --url http://disk-sophgo-vip.quickconnect.cn/sharing/1Z4DdzGsk && unzip other.zip && rm -rf other.zip && mv other models/
 
