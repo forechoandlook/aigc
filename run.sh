ea09ed26ae248a8c1ecc0e0bf5388056d840e483
forechoandlook <zuowangyang@foxmail.com>
2023-07-11 17:43:56 +0800

    add babes20

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-export BASENAME='babes20'
-echo "Please check babes20 in 'models' folder."
+# 如果 babses20 在 models/basic 下，那么export BASENAME='babes20'，否则export BASENAME='basic/deliberate-lora_pixarStyleLora_lora128-unet-2'
+if [ -d "./models/basic/babes20" ]; then
+     export BASENAME='babes20'
+fi
 export PYTHONPATH=$PYTHONPATH:/opt/sophon/sophon-opencv-latest/opencv-python/
 python3 app.py 
